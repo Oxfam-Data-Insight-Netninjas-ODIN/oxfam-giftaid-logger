81528a4544d57b68d1dea54e63b907a61f0fb326
docbogdanc <docbogdanc@yahoo.com>
2024-01-31 17:20:17 +0000

    create a function to save and load data from local storage according to the button pressed

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,24 @@ fullscreenButton.addEventListener("click", toggleFullscreen);
 function playSound(soundId) {
     const sound = document.getElementById(soundId);
     sound.play();
+}
+
+// create a function to increment the values according to which is button pressed
+function incrementCounter(buttonType) {
+    // create a variable referencing the curent date and change it to string
+        const currentDate = new Date().toLocaleDateString(); 
+        console.log("currenDate: "+ currentDate);
+        // create a variable for "clickData" stored in local storage  ; if no data then create an empty object
+        let data = JSON.parse(localStorage.getItem('clickData')) || {};
+        console.log("data : " +data);
+        // check if "currentDate" key exist in the saved "data" object ; if not, create a default value of 0
+        if (!data[currentDate]) {
+            data[currentDate] = { 'giftAid': 0, 'not': 0 };
+        }
+        // increase/update the value of the "data""currentDate" value of the button pressed
+        data[currentDate][buttonType]++;
+        console.log(data[currentDate]);
+        // save the updated data in local storage
+        localStorage.setItem('clickData', JSON.stringify(data));
+        updateTable();
+    }
