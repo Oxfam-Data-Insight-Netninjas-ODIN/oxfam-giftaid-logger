619236c5429deca61907135bef6843ee5b54a28a
docbogdanc <docbogdanc@yahoo.com>
2024-01-31 16:51:10 +0000

    added function to toggle fullscreen

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 // Back-end branch
+var isFullscreen = false;
+
+// create a function for toggle fullscreen view
+function toggleFullscreen() {
+    if (isFullscreen) {
+        document.exitFullscreen();
+    } else {
+        document.documentElement.requestFullscreen();
+    }
+    isFullscreen = !isFullscreen;
+}
