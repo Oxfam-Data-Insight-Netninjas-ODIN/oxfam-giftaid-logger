516dfd04bb6f277de8da5c66d4bb7edf1c2553b0
unknown <docbogdanc@yahoo.com>
2024-02-01 19:48:30 +0000

    history table is populated from js data

--- assets/tabledata.js
+++ assets/tabledata.js
@@ -7,32 +7,33 @@ var historyData = [
   { user: "F5678", firstName: "Duncan", giftAid: 3, not: 1, percentage: 75, date: "30/01/2023" },
   { user: "F2334", firstName: "Rob", giftAid: 4, not: 0, percentage: 100, date: "30/01/2023" },
 ];
-
+console.log(historyData);
 
 var bestUsers = [
     {}
 ];
 
-$(document).ready(function() {
-  // Attach a click event handler to the button
-  $('#myButton').click(function() {
-    // Open the other webpage
-    window.open('otherpage.html', '_blank');
-
+// variable for first object length
+var historyDataObjectLength = Object.keys(historyData).length;
+// loop through all elemeents of the object to and populate the table
+for (var i=0 ; i<historyDataObjectLength ; i++) {
+  console.log("loop is : "+historyDataObjectLength);
   
-    for (var i=0 ; i<historyData[0].length ; i++) {
-      console.log("loop is : "+historyData[0].length);
-      var populateInitials = storedHighScoredObj[i].key1;
-      var populateScore = storedHighScoredObj[i].key2;
-      if (populateInitials === undefined || populateScore === undefined ) {continue;}
-      if (populateInitials === null || populateScore === null ) {continue;}
-      var newRow = document.createElement("li");
-      newRow.innerHTML = populateInitials + " - " + populateScore;
-      highscores.appendChild(newRow);
-    }
+  var populateUser = historyData[i].user;
+  var populateName= historyData[i].firstName;
+  var populateGiftAid= historyData[i].giftAid;
+  var populateNot= historyData[i].not;
+  var populatePercentage= historyData[i].percentage;
+  var populateDate= historyData[i].date;
+  // if (populateInitials === undefined || populateScore === undefined ) {continue;}
+  // if (populateInitials === null || populateScore === null ) {continue;}
+  var newRow = $("<tr>");
+  $('#tableHistoryBody').append(newRow);
+  newRow.append(`<td>${populateDate}</td><td>${populateUser}</td><td>${populateName}</td><td>${populateGiftAid}</td><td>${populateNot}</td><td>${populatePercentage}</td>`)
+  $('#tableHistoryBody').append(newRow);
+
+}
 
 
-    // Run a JavaScript function from the other page
-    function populateTable () {}
-  });
-});
+// Run a JavaScript function from the other page
+function populateTable () {}
