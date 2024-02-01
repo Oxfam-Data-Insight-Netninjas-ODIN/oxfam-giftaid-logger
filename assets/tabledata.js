f36c90e60b8cad3a899ebf866f25ef6d0a7afdc7
unknown <docbogdanc@yahoo.com>
2024-02-01 19:07:54 +0000

    corrected links for css and js

--- assets/tabledata.js
+++ assets/tabledata.js
@@ -1,3 +1,6 @@
+var tableHistory = document.querySelector('#tableHistory');
+
+
 var historyData = [
   { user: "F2345", firstName: "John", giftAid: 2, not: 2, percentage: 50,  date: "30/01/2023" },
   { user: "F1234", firstName: "Martin", giftAid: 1, not: 1, percentage: 50, date: "30/01/2023" },
@@ -17,6 +20,18 @@ $(document).ready(function() {
     window.open('otherpage.html', '_blank');
 
   
+    for (var i=0 ; i<historyData[0].length ; i++) {
+      console.log("loop is : "+historyData[0].length);
+      var populateInitials = storedHighScoredObj[i].key1;
+      var populateScore = storedHighScoredObj[i].key2;
+      if (populateInitials === undefined || populateScore === undefined ) {continue;}
+      if (populateInitials === null || populateScore === null ) {continue;}
+      var newRow = document.createElement("li");
+      newRow.innerHTML = populateInitials + " - " + populateScore;
+      highscores.appendChild(newRow);
+    }
+
+
     // Run a JavaScript function from the other page
     function populateTable () {}
   });
